8b434a67577f94e74a4ea8b652b7b8b2f1d7b7eb
MinJung SEO <seominjung6052@gmail.com>
2022-05-30 13:22:25 +0900

    detail layout 구성

--- app/src/main/java/com/example/meetandmeet/ui/flower_detail/Flower_DetailFragment.java
+++ app/src/main/java/com/example/meetandmeet/ui/flower_detail/Flower_DetailFragment.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 
 import com.example.meetandmeet.R;
-
+//일단 기능 구현을 위해 adtivity로
 public class Flower_DetailFragment extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
