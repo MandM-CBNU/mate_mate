22273632a1c6b53870063ffb5dc9891ba8a002fe
sooyeonyim-t <yimsooyeon9@g.cbnu.ac.kr>
2022-06-15 06:11:22 +0900

    feat/conn_DB_no.1

--- app/src/main/java/com/example/meetandmeet/TaskListFragment.java
+++ app/src/main/java/com/example/meetandmeet/TaskListFragment.java
@@ -7,12 +7,17 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,18 +28,20 @@ import  com.example.meetandmeet.databinding.FragmentTaskListBinding;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class TaskListFragment extends Fragment {
     private FragmentTaskListBinding binding;
     private DetailsFragment detailsFragment = new DetailsFragment();
-    RecyclerViewAdapter recyclerViewAdapter;
+    static RecyclerViewAdapter recyclerViewAdapter;
     private static ArrayList<Item> list = new ArrayList<>();
-    // private static List<Item> list2 = new ArrayList<>();
     RecyclerView recyclerView;
     //데이터 베이스 선언
     private static DatabaseReference mDatabase;
     static Friends friend;
+    SharedViewModel viewModel;
+    RecyclerViewAdapter adapter;
 
     public TaskListFragment() {
         // Required empty public constructor
@@ -48,7 +55,28 @@ public class TaskListFragment extends Fragment {
         mDatabase = FirebaseDatabase.getInstance().getReference(); //DatabaseReference의 인스턴스
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_task_list, container, false);
         recyclerView = binding.recyclerView;
-        //list = createFriendsList();
+//        list = createFriendsList();
+//        list.add(new Item("이진희 씨의 화분",
+//                "010-3407-1511",
+//                "2020/06/07",
+//                0,
+//                R.drawable.firstpot));
+//        list.add(new Item("이민희 씨의 화분",
+//                "010-2315-1632",
+//                "2000/05/04",
+//                0,
+//                R.drawable.firstpot));
+//        list.add(new Item("임수연 씨의 화분",
+//                "456-654-6545",
+//                "2006/56/89",
+//                0,
+//                R.drawable.firstpot));
+//        list.add(new Item("서민정 씨의 화분",
+//                "121-654-6542",
+//                "2007/06/08",
+//                0,
+//                R.drawable.firstpot));
+
 
         return binding.getRoot();
     }
@@ -56,9 +84,15 @@ public class TaskListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-        SharedViewModel viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
         // list = createFriendsList();
+//        adapter.setListener((v,position) -> {
+//            viewModel.setSelected(adapter.getItemAt(position));
+//            getParentFragmentManager().beginTransaction().replace(R.id.framelayout,detailsFragment).addToBackStack(null).commit();
+//        });
+        list = createFriendsList();
 
+<<<<<<< Updated upstream
 
         list.add(new Item("이진희 씨의 화분",
                 "010-3407-1511",
@@ -83,17 +117,21 @@ public class TaskListFragment extends Fragment {
 
 
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+=======
+        adapter = new RecyclerViewAdapter(list);
+>>>>>>> Stashed changes
         binding.recyclerView.setAdapter(adapter);
         adapter.setListener((v,position) -> {
             viewModel.setSelected(adapter.getItemAt(position));
             getParentFragmentManager().beginTransaction().replace(R.id.framelayout,detailsFragment).addToBackStack(null).commit();
         });
+
     }
 
     //입력받은 숫자의 리스트 생성
     public ArrayList<Item> createFriendsList() {
 
-        ArrayList<Item> friends = new ArrayList<Item>();
+        ArrayList<Item> pots = new ArrayList<Item>();
 
         for(int n=1;n<=var_friend.i;n++){
             String s=String.valueOf(n);
@@ -101,12 +139,17 @@ public class TaskListFragment extends Fragment {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     friend = snapshot.getValue(Friends.class);
-                    friends.add(new Item(friend.name,friend.phone,friend.birth,friend.progressNum,R.drawable.blankpot));
+                    pots.add(new Item(friend.name,friend.phone,friend.birth,friend.progressNum,friend.imgUrl));
                     //이 리스트들을 어댑터에 넣어진다.
-                    recyclerView.setHasFixedSize(true);
-                    recyclerViewAdapter = new RecyclerViewAdapter(list);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-                    recyclerView.setAdapter(recyclerViewAdapter);
+
+                    binding.recyclerView.setHasFixedSize(true);
+                    adapter = new RecyclerViewAdapter(list);
+                    recyclerView.setAdapter(adapter);
+                    adapter.setListener((v,position) -> {
+                        viewModel.setSelected(adapter.getItemAt(position));
+                        getParentFragmentManager().beginTransaction().replace(R.id.framelayout,detailsFragment).addToBackStack(null).commit();
+                    });
+
                 }
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) { //참조에 액세스 할 수 없을 때 호출
@@ -114,6 +157,7 @@ public class TaskListFragment extends Fragment {
             });
 
         }
-        return friends;
+        return pots;
     }
 }
+
